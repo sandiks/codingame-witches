5d6448b46d4cf3abe8142217f338f61643a85c88
Mikalai Asipenka <mika@opinov8.com>
2020-11-13 15:39:20 +0200

    fix files

--- helper.rb
+++ helper.rb
@@ -1,37 +1,82 @@
-
-
-def  parse_data_f
-    lines = File.readlines("data_test.txt")
-  
-    @players =[]
-    @recipes=[]
-    
-    action_count = lines[0].to_i
-    action_count.times do |idx|
-      action_id, action_type, d0, d1, d2, d3, price, tome_index, tax_count, castable, repeatable = lines[idx+1].split(" ")
-      r = Recipe.new
-      r.aid = action_id.to_i
-      r.atype = action_type
-      r.ingreds = [d0, d1, d2, d3].map{|x| x.to_i}
-
-      r.price = price.to_i
-      r.tome_index = tome_index.to_i
-      r.tax_count = tax_count.to_i
-      r.castable = castable.to_i == 1
-      r.repeatable = repeatable.to_i == 1
-      r.total_worth = r.ingreds.mul([10.0, 20.0, 30.0, 40.0])
-      @recipes << r
+class Array
+  def mul(factors)
+    sum = 0
+    (0..length - 1).each do |i|
+      sum += self[i] * factors[i]
     end
-
-    2.times do |idx|
-        # inv_0: tier-0 ingredients in inventory
-        # score: amount of rupees
-        inv_0, inv_1, inv_2, inv_3, score = lines[idx + action_count+1].split(" ").collect { |x| x.to_i }
-        p = Player.new
-        p.inv = [inv_0,inv_1,inv_2,inv_3]
-        p.score = score
-        @players << p
-    end
-  
+    sum
+  end
 end
-  
+
+class Player
+  attr_accessor :inv, :score, :brew_recipes, :spells
+end
+
+class Recipe
+  attr_accessor :aid, :atype, :ingreds, :price, :tome_index, :tax_count, :castable, :repeatable, :total_worth
+end
+
+def parse_input_data
+  @players = []
+  @recipes = []
+
+  action_count = gets.to_i
+  action_count.times do |_idx|
+    action_id, action_type, d0, d1, d2, d3, price, tome_index, tax_count, castable, repeatable = gets.split(' ')
+    r = Recipe.new
+    r.aid = action_id.to_i
+    r.atype = action_type
+    r.ingreds = [d0, d1, d2, d3].map { |x| x.to_i }
+
+    r.price = price.to_i
+    r.tome_index = tome_index.to_i
+    r.tax_count = tax_count.to_i
+    r.castable = castable.to_i == 1
+    r.repeatable = repeatable.to_i == 1
+    @recipes << r
+  end
+
+  2.times do |_idx|
+    # inv_0: tier-0 ingredients in inventory
+    # score: amount of rupees
+    inv_0, inv_1, inv_2, inv_3, score = gets.split(' ').collect { |x| x.to_i }
+    pl = Player.new
+    pl.inv = [inv_0, inv_1, inv_2, inv_3]
+    pl.score = score
+    @players << pl
+  end
+end
+
+def parse_data_f
+  lines = File.readlines('data_test.txt')
+
+  @players = []
+  @recipes = []
+
+  action_count = lines[0].to_i
+  action_count.times do |idx|
+    action_id, action_type, d0, d1, d2, d3, price, tome_index, tax_count, castable, repeatable = lines[idx + 1].split(' ')
+    r = Recipe.new
+    r.aid = action_id.to_i
+    r.atype = action_type
+    r.ingreds = [d0, d1, d2, d3].map { |x| x.to_i }
+
+    r.price = price.to_i
+    r.tome_index = tome_index.to_i
+    r.tax_count = tax_count.to_i
+    r.castable = castable.to_i == 1
+    r.repeatable = repeatable.to_i == 1
+    r.total_worth = r.ingreds.mul([10.0, 20.0, 30.0, 40.0])
+    @recipes << r
+  end
+
+  2.times do |idx|
+    # inv_0: tier-0 ingredients in inventory
+    # score: amount of rupees
+    inv_0, inv_1, inv_2, inv_3, score = lines[idx + action_count + 1].split(' ').collect { |x| x.to_i }
+    p = Player.new
+    p.inv = [inv_0, inv_1, inv_2, inv_3]
+    p.score = score
+    @players << p
+  end
+end
--- main.rb
+++ main.rb
@@ -3,127 +3,69 @@ STDOUT.sync = true # DO NOT REMOVE
 # the standard input according to the problem statement.
 require_relative 'helper'
 
-class Array
+class Game
+  attr_accessor :players, :recipes, :testing
 
-  def mul(factors)
-    sum =0
-    for i in 0..self.length-1
-      sum += self[i] * factors[i]
-    end
-    sum
+  def initialize
+    @factors = [10.0, 20.0, 30.0, 40.0]
   end
 
-end
+  def parse_input_data_and_init_some_variables
+    @my = @players[0]
+    @brew_recipes = @recipes.select { |s| s.atype == 'BREW' }
 
-
-class Player
-  attr_accessor :inv, :score
-  attr_accessor :brew_recipes, :spells
-end
-
-class Recipe
-  attr_accessor :aid, :atype, :ingreds, :price, :tome_index, :tax_count, :castable, :repeatable 
-  attr_accessor :total_worth
-end
-
-class Game
-    attr_accessor :players, :recipes, :testing
-    
-    def initialize()
-      @factors = [10.0, 20.0, 30.0, 40.0]
-    end
-    
-    def  parse_input_data
-      
-        @players =[]
-        @recipes=[]
-        
-        action_count = gets.to_i
-        action_count.times do |idx|
-          action_id, action_type, d0, d1, d2, d3, price, tome_index, tax_count, castable, repeatable = gets.split(" ")
-          r = Recipe.new
-          r.aid = action_id.to_i
-          r.atype = action_type
-          r.ingreds = [d0, d1, d2, d3].map{|x| x.to_i}
-
-          r.price = price.to_i
-          r.tome_index = tome_index.to_i
-          r.tax_count = tax_count.to_i
-          r.castable = castable.to_i == 1
-          r.repeatable = repeatable.to_i == 1
-          @recipes << r
-        end
-    
-        2.times do |idx|
-            # inv_0: tier-0 ingredients in inventory
-            # score: amount of rupees
-            inv_0, inv_1, inv_2, inv_3, score = gets.split(" ").collect { |x| x.to_i }
-            pl = Player.new
-            pl.inv = [inv_0,inv_1,inv_2,inv_3]
-            pl.score = score
-            @players << pl
-        end
-    end
-  
-    def parse_input_data_and_init_some_variables
-      @my = @players[0]
-      @brew_recipes = @recipes.select{|s| s.atype =="BREW"}
-
-      @my.spells = @recipes.select{|s| s.atype =="CAST"}
-      @opp = @players[1]
-      @opp.spells = @recipes.select{|s| s.atype =="OPPONENT_CAST"}
-    end
+    @my.spells = @recipes.select { |s| s.atype == 'CAST' }
+    @opp = @players[1]
+    @opp.spells = @recipes.select { |s| s.atype == 'OPPONENT_CAST' }
+  end
 
   def run
-    round=0
+    round = 0
     loop do
-      round+=1
-      break if round>2
+      round += 1
+      break if round > 2
 
       @testing ? parse_data_f : parse_input_data
 
       parse_input_data_and_init_some_variables
 
-      printf("----players info #{round}
-        my inventory #{@my.inv}
-        opp inventory #{@opp.inv}\n") if @testing
-  
+      if @testing
+        printf("----players info #{round}
+          my inventory #{@my.inv}
+          opp inventory #{@opp.inv}\n")
+      end
 
       rr = find_match_recipes
       best = select_best_recipe(rr)
-      if best 
+      if best
         printf("BREW #{best.aid}\n")
       else
         printf("WAIT \n")
       end
-
     end
   end
 
-  def can_use_recipe(a,b)
+  def can_use_recipe(a, b)
     a[0] >= -b[0] && a[1] >= -b[1] && a[2] >= -b[2] && a[3] >= -b[3]
   end
-  
 
-  
   def find_match_recipes
     inv = @my.inv
     p player_factor = @my.inv.mul(@factors)
 
     @brew_recipes.each do |rr|
-      rr.total_worth = -rr.ingreds.mul(@factors)/player_factor * rr.price
+      rr.total_worth = -rr.ingreds.mul(@factors) / player_factor * rr.price
     end
 
-    sorted = @brew_recipes.sort_by{|x| x.total_worth}
-    p sorted.map{|rr| [rr.aid,rr.ingreds, rr.total_worth]}
-    
-    matched = sorted.select { |rr| can_use_recipe(inv,rr.ingreds) }
+    sorted = @brew_recipes.sort_by { |x| x.total_worth }
+    p sorted.map { |rr| [rr.aid, rr.ingreds, rr.total_worth] }
+
+    matched = sorted.select { |rr| can_use_recipe(inv, rr.ingreds) }
   end
 
   def select_best_recipe(matched)
     matched.first
   end
-
 end
 
 g = Game.new
